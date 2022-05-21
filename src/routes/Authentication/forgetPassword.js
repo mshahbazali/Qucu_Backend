6f0a966e00779a90cc558e9a598d4e5df893a6d9
Shahbaz Ali <75558726+mshahbazali@users.noreply.github.com>
2022-05-21 10:04:27 -0700

    Forgot Api Update

--- src/routes/Authentication/forgetPassword.js
+++ src/routes/Authentication/forgetPassword.js
@@ -6,54 +6,48 @@ const accountSid = "ACb6fb8253fb7d40091b2900d74907b33e";
 const authToken = "fc1fff0d78092aebde9de442b1fc40e2";
 const authPhoneNumber = "+19403735129";
 const client = require('twilio')(accountSid, authToken);
+
+
 router.post("/forgotpassword", async (req, res) => {
     try {
-        const phoneNumber = req.body.identifier
-        authSchema.findOne({ phoneNumber: phoneNumber })
-            .exec()
-            .then(async (user) => {
-                if (user.length < 1) {
-                    res.status(202).send({
-                        massage: "Please enter valid mobile number"
-                    })
-                }
-                else {
-                    const _id = user[0]._id.toString();
-                    const otp = Math.floor(Math.random() * 92) * 64;
-                    req.body.otp = otp;
-                    const updateauth = await authSchema.findByIdAndUpdate(_id, req.body, {
-                        new: true
-                    })
-                    client.messages
-                        .create({
-                            to: phoneNumber,
-                            from: authPhoneNumber,
-                            body: `Your QUCU verification code is: ${otp}`,
-                        })
-                        .then(message => console.log(message.sid)).catch((err) => console.log(err))
-                    setTimeout(async () => {
-                        req.body.otp = null;
-                        const updateauth = await authSchema.findByIdAndUpdate(_id, req.body, {
-                            new: true
-                        })
-                    }, 300000);
-
-                }
+        let user = await authSchema.findOne({ phoneNumber: req.body.identifier });
+        console.log(user);
+        if (user == null) {
+            res.status(202).send({
+                massage: "Please enter valid mobile number"
             })
-            .catch(e => {
-                res.status(404).send({
-                    massage: "User Not Found"
+        }
+        else {
+            const _id = user._id.toString();
+            const otp = Math.floor(Math.random() * 92) * 64;
+            req.body.otp = otp;
+            const updateauth = await authSchema.findByIdAndUpdate(_id, req.body, {
+                new: true
+            })
+            res.status(202).send(updateauth)
+            client.messages
+                .create({
+                    to: phoneNumber,
+                    from: authPhoneNumber,
+                    body: `Your QUCU verification code is: ${otp}`,
                 })
-            })
+                .then(message => console.log(message.sid)).catch((err) => console.log(err))
+            setTimeout(async () => {
+                req.body.otp = null;
+                const updateauth = await authSchema.findByIdAndUpdate(_id, req.body, {
+                    new: true
+                })
+            }, 300000);
+        }
     }
-    catch (e) {
-        res.status(400).send(e)
+    catch (err) {
+        res.status(400).send({ err: err })
     }
 })
 router.post("/verify", async (req, res) => {
     try {
         let user = await authSchema.findOne({ otp: req.body.otp });
-        if (!user) {
+        if (user == null) {
             return res.status(202).send({ message: 'Your One Time Verification Code is Invalid' });
         } else {
             res.status(202).send({
@@ -69,35 +63,27 @@ router.post("/verify", async (req, res) => {
 })
 router.post("/reset-password", async (req, res) => {
     try {
-        authSchema.findById({ _id: req.body.id })
-            .exec()
-            .then(async (user) => {
-                if (user.length < 1) {
-                    res.status(404).send({
-                        massage: "User Not Found"
-                    })
-                }
-                else {
-                    const _id = req.body.id
-                    const securePass = await bcrypt.hash(req.body.password, 10);
-                    req.body.password = securePass
-                    const updateauth = await authSchema.findByIdAndUpdate(_id, req.body, {
-                        new: true
-                    }).then(() => {
-                        res.status(201).send({
-                            message: "Your password successfully reset",
-                        })
-                    })
-                }
+        let user = await authSchema.findOne({ _id: req.body.otp });
+        if (user == null) {
+            res.status(404).send({
+                massage: "User Not Found"
             })
-            .catch(e => {
-                res.status(404).send({
-                    massage: "User Not Found"
+        }
+        else {
+            const _id = req.body.id
+            const securePass = await bcrypt.hash(req.body.password, 10);
+            req.body.password = securePass
+            const updateauth = await authSchema.findByIdAndUpdate(_id, req.body, {
+                new: true
+            }).then(() => {
+                res.status(201).send({
+                    message: "Your password successfully reset",
                 })
             })
+        }
     }
-    catch (e) {
-        res.status(400).send(e)
+    catch (err) {
+        res.status(400).send(err)
     }
 })
 
